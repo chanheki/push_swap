13a12b7984ae3b7f1292643ff5ad385459c56038
chanhihi <chanhihi55@gmail.com>
2022-11-30 19:08:37 +0900

    PID

--- push_swap.c
+++ push_swap.c
@@ -24,22 +24,6 @@ int	main(int argc, char **argv)
 	
 	//TODO: business
 
-	t_stack *pA = &stack_a;
-	t_stack *pB = &stack_b;
-
-	swap(pA,pB,STACK_A);
-	show_stack_top_bottom(pA);
-	show_stack(pA);
-
-	// push(pA,pB,STACK_B);
-	push(pA,pB,STACK_B);
-	printf("stack B -> \n");
-	show_stack_top_bottom(pB);
-	show_stack(pB);
-	printf("stack A -> \n");
-	show_stack_top_bottom(pA);
-	show_stack(pA);
-
-	printf("E N D P R O G R A M \n");
+	printf("- E N D P R O G R A M - \n");
 	return (0);
 }
